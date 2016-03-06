4361d6578676a19d9618fdcd1f48a054d0cbedbc
ozitraveller <ozitraveller#gmail.com>
2016-03-07 06:20:10 +1100

    Add i3, also dmenu to all

--- jwm-64/config/includes.chroot/etc/skel/bin/run.sh
+++ jwm-64/config/includes.chroot/etc/skel/bin/run.sh
@@ -6,20 +6,19 @@
 #pgrep wicd-client &>/dev/null; [ $? = 0 ] && killall wicd-client
 pkill wicd-client
 pkill conky
-pkill clipit
+# pkill clipit
 
 # before nitrogen
 hsetroot -solid "#000000" & 
 
 sleep 1
 {
-	compton --config ~/.compton.conf &
+#	compton --config ~/.compton.conf &
 	
 	(sleep 2 && wicd-gtk --tray) &
-#	(sleep 2 && xbattbar-acpi -s 2 -p left -I "dark green" -O "black") &
 #	(sleep 3 && volumeicon --tray) &
 	(conky --pause=3 -c ~/.conkyrc) &
-	(sleep 3 && clipit) &
+#	(sleep 3 && clipit) &
 	nitrogen --restore &
 	(sleep 4 && star-welcome --firstrun) &
 }
---
+++ jwm-noDM-64/config/includes.chroot/etc/skel/bin/.conky/.conky-startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# sh /home/simon/.conky-startup.sh
+
+# (conky --pause=3 -c ~/bin/.conky/.conkyrc.main) &
+# (conky --pause=3 -c ~/bin/.conky/.conkyrc.mini) &
+(conky --pause=1 -c ~/bin/.conky/.conkyrc.ribbon) &
+# (conky --pause=3 -c ~/bin/.conky/.conkyrc.keys) & 
+# (conky --pause=3 -c ~/bin/.conky/.conkyrc.clock) & 
+
